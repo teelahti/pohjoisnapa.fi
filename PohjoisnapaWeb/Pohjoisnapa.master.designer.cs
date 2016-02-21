7f9d8d848b82bdfa4cccb5dedda779284f2aa33c
Tero Teelahti <tero@teelahti.fi>
2016-02-21 14:21:24 +0200

    Change startup properties

--- PohjoisnapaWeb/Pohjoisnapa.master.designer.cs
+++ PohjoisnapaWeb/Pohjoisnapa.master.designer.cs
@@ -57,6 +57,15 @@ public partial class Pohjoisnapa {
     protected global::System.Web.UI.WebControls.Image Fiiliskuva;
     
     /// <summary>
+    /// Kartta control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.Image Kartta;
+    
+    /// <summary>
     /// Mainmenu control.
     /// </summary>
     /// <remarks>
