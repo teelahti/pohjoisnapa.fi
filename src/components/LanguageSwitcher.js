9c4d4e496da14dbbf85a1518d4804f335812b129
Tero Teelahti <tero@armadainteractive.com>
2018-02-14 20:29:12 +0200

    Fix language switcher class name

--- src/components/LanguageSwitcher.js
+++ src/components/LanguageSwitcher.js
@@ -1,15 +1,12 @@
 import React, { Component } from "react";
+import classNames from "classnames";
 import { translate } from "react-i18next";
 
 class LanguageSwitcher extends Component {
   constructor(props) {
     super(props);
-    const {
-      i18n
-    } = this.props;
-    this.state = {
-      language: i18n.language
-    };
+    const { i18n } = this.props;
+    this.state = { language: i18n.language };
 
     this.handleChangeLanguage = this.handleChangeLanguage.bind(this);
   }
@@ -21,16 +18,14 @@ class LanguageSwitcher extends Component {
   }
 
   handleChangeLanguage(lng) {
-    const {
-      i18n
-    } = this.props;
+    const { i18n } = this.props;
     i18n.changeLanguage(lng);
   }
 
   renderLanguageChoice({ code, label }) {
-    const buttonClass = {
+    const buttonClass = classNames("LanguageSwitcher__button", {
       "LanguageSwitcher__button--selected": this.state.language === code,
-    };
+    });
 
     return (<button key={code} className={buttonClass} onClick={() => this.handleChangeLanguage(code)} > {
       label
