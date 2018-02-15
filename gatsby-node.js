007d105b5594b70ba3a613685ecea8e180b5d7cc
Tero Teelahti <tero@armadainteractive.com>
2018-02-15 07:31:23 +0200

    Convert old resource files to json translation files. Clean up files that are not needed

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,13 +4,4 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-const fs = require("fs-extra");
-const path = require("path");
-
-exports.onPostBuild = () => {
-  console.log("Copying locales");
-  fs.copySync(
-    path.join(__dirname, "/src/locales"),
-    path.join(__dirname, "/public/locales")
-  );
-};
+// You can delete this file if you're not using it
